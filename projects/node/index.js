c7ad2f3fa3e2085ad0436e3585047a16245709af
MARKASCHARAN <markascharan@gmail.com>
2025-04-29 09:16:45 +0530

    adv docker

--- projects/node/index.js
+++ projects/node/index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('Hello World, Charansss');
+    res.send('Hello World, Charanss!');
 })
 
 app.listen(3000);
